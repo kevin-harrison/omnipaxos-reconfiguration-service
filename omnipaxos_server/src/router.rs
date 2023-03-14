3c2fa5ea50386a09e897be08b0f52ecc30fba739
kevin <kevinha@kth.se>
2023-03-14 18:23:13 +0100

    Single OmniPaxos instance fail-stop with simple client

--- omnipaxos_server/src/router.rs
+++ omnipaxos_server/src/router.rs
@@ -4,24 +4,16 @@ use tokio::net::{TcpStream, TcpListener, TcpSocket};
 use tokio_serde::{formats::{Json, Cbor}, Framed};
 use tokio_util::codec::{Framed as CodecFramed, LengthDelimitedCodec};
 use anyhow::{anyhow, Error};
-use std::{pin::Pin, collections::HashMap, net::SocketAddr};
+use std::{pin::Pin, collections::{HashMap, VecDeque}, net::SocketAddr};
 use std::task::{Context, Poll};
 
 use omnipaxos_core::{util::NodeId, messages::Message};
 use crate::message::{NodeMessage, ClientMessage, ServerMessage};
 use crate::kv::{KVSnapshot, KeyValue};
 
+
 use std::mem;
 use log::*;
-
-/*
-type FramedServer = Framed<
-    CodecFramed<TcpStream, LengthDelimitedCodec>,
-    Value,
-    Value,
-    Json<Value, Value>,
->;
-*/
 
 type NodeConnection = Framed<
     CodecFramed<TcpStream, LengthDelimitedCodec>,
@@ -30,25 +22,26 @@ type NodeConnection = Framed<
     Cbor<NodeMessage, NodeMessage>,
 >;
 
-
 pub struct Router {
+    id: NodeId,
     listener: TcpListener,
     nodes: HashMap<NodeId, NodeConnection>,
     node_addresses: HashMap<NodeId, SocketAddr>,
     pending_nodes: Vec<NodeConnection>,
-    buffer: Vec<NodeMessage>
+    buffer: VecDeque<NodeMessage>
 }
 
 impl Router {
-    pub async fn new(addr: SocketAddr, addresses: HashMap<NodeId, SocketAddr>) -> Result<Self, Error> {
-        let listener = TcpListener::bind(addr).await?;
+    pub async fn new(id: NodeId, listen_address: SocketAddr, addresses: HashMap<NodeId, SocketAddr>) -> Result<Self, Error> {
+        let listener = TcpListener::bind(listen_address).await?;
 
         Ok(Self {
+            id,
             listener,
             nodes: HashMap::new(),
             node_addresses: addresses,
             pending_nodes: vec![],
-            buffer: vec![]
+            buffer: VecDeque::new()
         })
     }
     
@@ -58,11 +51,9 @@ impl Router {
         } else {
             // No connection to node so if heartbeat message try to reconnect then send
             if let NodeMessage::OmniPaxosMessage(omnipaxos_core::messages::Message::BLE(_)) = msg {
-                debug!("Trying to reconnect to node");
+                trace!("Trying to reconnect to node");
                 self.add_node(node).await?;
-                self.nodes.get_mut(&node).unwrap().send(msg).await?; // TODO: Need to called
-                                                                         // reconnected on
-                                                                         // reconnects 
+                self.nodes.get_mut(&node).unwrap().send(msg).await?;
             }
             return Err(anyhow!("Node `{}` not connected!", node));
         }
@@ -71,26 +62,14 @@ impl Router {
 
     async fn add_node(&mut self, node: NodeId) -> Result<(), Error> {
         if let Some(address) = self.node_addresses.get(&node) {
-            //let socket = TcpSocket::new_v4()?;
-            //socket.bind(self.listener.local_addr()?)?;
-            let tcp_stream = self.listener.connect(*address).await?; 
+            let tcp_stream = TcpStream::connect(address).await?; 
             let length_delimited = CodecFramed::new(tcp_stream, LengthDelimitedCodec::new());
-            let framed = Framed::new(length_delimited, Cbor::default());
+            let mut framed = Framed::new(length_delimited, Cbor::default());
+            framed.send(NodeMessage::Hello(self.id)).await?;
             self.nodes.insert(node, framed);
             return Ok(());
         }
         Err(anyhow!("No known address for node {}", node))
-    }
-    
-    fn add_connection(&mut self, socket: TcpStream, address: SocketAddr) -> Result<(), Error> {
-        let node_id = self.node_addresses.iter().find_map(|(id, addr)| if *addr == address {Some(id)} else {None});
-        if let Some(&id) = node_id {
-            let length_delimited = CodecFramed::new(socket, LengthDelimitedCodec::new());
-            let framed = Framed::new(length_delimited, Cbor::default());
-            self.nodes.insert(id, framed);
-            return Ok(());
-        }
-        Err(anyhow!("No known node for address {}", address))
     }
 }
 
@@ -104,19 +83,18 @@ impl Stream for Router {
         if let Poll::Ready(val) = Pin::new(&mut self_mut.listener).poll_accept(cx) {
             match val {
                 Ok((tcp_stream, socket_addr)) => {
-                    match self_mut.add_connection(tcp_stream, socket_addr) {
-                        Err(err) => error!("Error adding new connection:{:?}", err),
-                        _ => debug!("CONNECTED TO {}", socket_addr),
-                    }
+                    debug!("New connection from {}", socket_addr);
+                    let length_delimited = CodecFramed::new(tcp_stream, LengthDelimitedCodec::new());
+                    let framed = Framed::new(length_delimited, Cbor::default());
+                    self_mut.pending_nodes.push(framed);
                 }
                 Err(err) => {
-                    error!("Error checking for new requests:{:?}", err);
+                    error!("Error checking for new requests: {:?}", err);
                     return Poll::Ready(None); // End stream
                 }
             }
         }
-
-        /* 
+ 
         let mut new_pending = Vec::new();
 
         mem::swap(&mut self_mut.pending_nodes, &mut new_pending);
@@ -125,23 +103,19 @@ impl Stream for Router {
             if let Poll::Ready(val) = Pin::new(&mut pending).poll_next(cx) {
                 match val {
                     Some(Ok(NodeMessage::Hello(id))) => {
-                        debug!("New Node connection from `{}`",id);
-                        self_mut.buffer.push(NodeMessage::Hello(id));
+                        debug!("Node {} handshake complete",id);
+                        self_mut.buffer.push_back(NodeMessage::Hello(id));
                         self_mut.nodes.insert(id, pending);
                     }
-                    Some(Ok(msg)) => {
-                        warn!("Received unknown message during handshake:{:?}", msg);
-                    }
-                    Some(Err(err)) => {
-                        error!("Error checking for new requests:{:?}", err);
-                    }
+                    Some(Ok(NodeMessage::Append(kv))) => self_mut.buffer.push_back(NodeMessage::Append(kv)),
+                    Some(Ok(msg)) => warn!("Received unknown message during handshake: {:?}", msg),
+                    Some(Err(err)) => error!("Error checking for new requests: {:?}", err),
                     None => (),
                 }
             } else {
                 self_mut.pending_nodes.push(pending);
             }
         }
-        */
 
 
         let mut new_nodes = HashMap::new();
@@ -152,7 +126,7 @@ impl Stream for Router {
             match Pin::new(&mut client).poll_next(cx) {
                 Poll::Ready(Some(Ok(val))) => {
                     trace!("Received message from node `{}`: {:?}", name, val);
-                    self_mut.buffer.push(val);
+                    self_mut.buffer.push_back(val);
                     self_mut.nodes.insert(name, client);
                 }
                 Poll::Ready(None) => {
@@ -170,7 +144,7 @@ impl Stream for Router {
         }
 
 
-        if let Some(val) = self_mut.buffer.pop() {
+        if let Some(val) = self_mut.buffer.pop_front() {
             if self_mut.buffer.len() > 0 {
                 cx.waker().wake_by_ref();
             }
